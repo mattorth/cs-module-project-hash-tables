ed9d281229e0a88a99981886ea8390dd82984536
mattorth <orth.mj@gmail.com>
2020-11-05 20:04:25 -0600

    word count done

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,22 @@
+import re
 def word_count(s):
     # Your code here
+    dict = {}
+    characters = '[\"\:\;\,\.\-\+\=\/\\\|[\]\{\}\(\)\*\^\&]'
+    lowercase = s.lower()
+    if len(lowercase) == 0:
+        return dict
+    lower = re.sub(characters, '', lowercase).split()
 
+    if len(lower)== 0:
+        return dict
+
+    for word in lower:
+        if word in dict:
+            dict[word] += 1
+        else:
+            dict[word] = 1
+    return dict
 
 
 if __name__ == "__main__":
